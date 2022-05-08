e8b37b6a611ebc89fbe545948a193798581b6c10
josnethmoreno <josnethmoreno@gmail.com>
2022-05-08 12:53:35 -0400

    Starting project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Header from './components/sections/Header'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/sections/Header.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Header() {
+	return(
+		<div className="Header">
+			Header
+		</div>
+	);
+}
